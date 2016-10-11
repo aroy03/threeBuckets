53988d6d32715aa51286fcead7a6f6b80d81e09d
aroy03 <aparnaroy@gmail.com>
2016-10-10 17:18:20 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,36 @@ App = function()
 	this.init = function()
 	{
         // load a scene
-		wade.loadScene('scene1.wsc', true, function()
+	wade.loadScene('scene1.wsc', true, function()
         {
             // the scene has been loaded, do something here
 
         });
+		
+	// create a red square
+        var sprite = new Sprite();
+        sprite.setSize(100, 100);
+        sprite.setDrawFunction(wade.drawFunctions.solidFill_('red'));
+        this.square = new SceneObject(sprite);
+        wade.addSceneObject(this.square);
+ 
+
+	    this.onDeviceOrientation = function(eventData)
+	    {
+		// calculate new position
+		var pos = this.square.getPosition();
+		pos.x += eventData.beta;
+		pos.y -= eventData.gamma;
+
+		// restrict new position to the screen
+		var w = wade.getScreenWidth()  / 2 - 50;
+		var h = wade.getScreenHeight() / 2 - 50;
+		pos.x = Math.min(Math.max(pos.x, -w), w);
+		pos.y = Math.min(Math.max(pos.y, -h), h);
+
+		// update the position of the square
+		this.square.setPosition(pos.x, pos.y);
+	    };
         
         wade.app.onMouseUp = function()
         {
